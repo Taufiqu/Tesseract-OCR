0e1d677417108c6b08c20b674ba2db8927ee8fb1
Hafizh <112592675+Taufiqu@users.noreply.github.com>
2025-07-14 18:43:29 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -105,6 +105,6 @@ def serve_preview(filename):
 # ==============================================================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     print(f"🚀 Running Flask on http://0.0.0.0:{port}")
     app.run(host="0.0.0.0", port=port)
